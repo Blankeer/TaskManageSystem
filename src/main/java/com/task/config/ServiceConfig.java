d2fcffa3851c3b5cd72cc39e6cf8159a41857f5e
blanke-x11 <blankeeee@gmail.com>
2017-01-26 16:33:21 +0800

    [ADD]Config:add ServiceConfig class.

---
+++ src/main/java/com/task/config/ServiceConfig.java
@@ -0,0 +1,12 @@
+package com.task.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by blanke on 17-1-25.
+ */
+@Configuration
+@ComponentScan("com.task")
+public class ServiceConfig {
+}
