d49927f44babdcfe3fef8d70eadf77aa5b001825
blanke-x11 <blankeeee@gmail.com>
2017-01-27 06:07:53 +0800

    [ADD]Field:add field logic.

---
+++ src/main/java/com/task/repository/FieldRepository.java
@@ -0,0 +1,11 @@
+package com.task.repository;
+
+import com.task.bean.Field;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by blanke on 17-1-25.
+ */
+public interface FieldRepository extends JpaRepository<Field, Integer> {
+
+}
