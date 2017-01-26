daaf6b90b2ee6e8f427cd37dd6731b214480e5d8
blanke-x11 <blankeeee@gmail.com>
2017-01-26 16:30:09 +0800

    [ADD]utils:add Md5 and text Utils.

---
+++ src/main/java/com/task/utils/Md5Utils.java
@@ -0,0 +1,10 @@
+package com.task.utils;
+
+/**
+ * Created by blanke on 17-1-26.
+ */
+public class Md5Utils {
+    public static String md5(String text) {
+        return text;// TODO: 17-1-26  
+    }
+}
---
+++ src/main/java/com/task/utils/TextUtils.java
@@ -0,0 +1,10 @@
+package com.task.utils;
+
+/**
+ * Created by blanke on 17-1-25.
+ */
+public class TextUtils {
+    public static boolean isEmpty(String text) {
+        return text == null || text.trim().length() == 0;
+    }
+}
