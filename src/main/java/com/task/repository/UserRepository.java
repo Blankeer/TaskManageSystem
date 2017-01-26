1f046d47df0e50b9ac2be2c251471a6362472f55
blanke-x11 <blankeeee@gmail.com>
2017-01-26 17:37:19 +0800

    [ADD]Repository:add UserRepository.

--- src/main/java/com/task/repository/UserRepository.java
+++ src/main/java/com/task/repository/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created by blanke on 17-1-25.
  */
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByEmail(String email);
 }
