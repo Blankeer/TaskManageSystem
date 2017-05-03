2fee3c6cd6ec1abe1953a5c83c9ddffeba63e359
blanke <blankeeee@gmail.com>
2017-05-04 01:51:36 +0800

    [IMP]  修改 user_list page

--- src/main/resources/static/admin/js/users_list.js
+++ src/main/resources/static/admin/js/users_list.js
@@ -18,20 +18,28 @@ $(document).ready(function () {
                     'nickName': $('#dialog_user_nickname').val(),
                 };
                 $.put('/users/' + click_user_id, data, function () {
-                    //TODO
+                    $.msg_success("修改成功");
+                    reLoad();
                 });
             });
             //删除
             $('#user_delete').click(function () {
                 $.delete('/users/' + click_user_id, function () {
-                    //TODO
+                    $.msg_success("删除成功");
+                    reLoad();
                 })
             })
         }
     });
 });
+//重新加载该页面
+function reLoad() {
+    $('#addUserDialog').modal('hide');
+    initPagination();
+}
 //初始化分页
 function initPagination() {
+    $('#pagination').twbsPagination('destroy');
     getUsers(0, size, function (data) {
         $('#pagination').twbsPagination({
             totalPages: data.totalPages,
@@ -50,9 +58,6 @@ function initPagination() {
                 });
             }
         });
-        for (var i in data.content) {
-            addUserRowHtml(data.content[i]);
-        }
     });
 }
 //ajax 之后,把 数据转换成 html, 添加到页面
@@ -61,13 +66,14 @@ function addUserRowHtml(item_data) {
 }
 //解析 ajax ,返回每行数据
 function getUserRowItem(data) {
-    var item_html = $("<div></div>");
+    var item_html = $("#user_item_template").clone();
     item_html.attr('id', getUserViewId(data.id));
-    var item = $("<div></div>");
-    item.addClass('user_item');
-    var item_title = item.clone();
-    item_title.text(data.email);
-    item_html.append(item_title);
+    item_html.addClass('users_item');
+    var user_name = item_html.find('.user_name');
+    var user_email = item_html.find('.user_email');
+    user_name.text(data.nickName);
+    user_email.text(data.email);
+    item_html.show();
     //点击每行
     item_html.click(function () {
         click_user_id = data.id;
