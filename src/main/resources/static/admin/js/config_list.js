f3263e82580a511d5be7f43abb25384d254cf754
blanke <blankeeee@gmail.com>
2017-05-04 01:41:58 +0800

    [IMP]  修改 config page

--- src/main/resources/static/admin/js/config_list.js
+++ src/main/resources/static/admin/js/config_list.js
@@ -3,8 +3,6 @@ var click_config_id = -1;
 $(document).ready(function () {
     $('#bu_config_add').click(function () {
         click_config_id = -1;//清空
-        // localStorage.removeItem('click_task_id');//新增
-        // $('#menuFrame', parent.document.body).attr('src', 'task_add.html')
     });
 
     initPagination();
@@ -27,17 +25,22 @@ $(document).ready(function () {
                     'expression': $('#dialog_config_reg').val()
                 };
                 $.put('/configs/' + click_config_id, data, function () {
-                    //TODO
+                    $.msg_success("修改成功");
+                    reLoad();
                 });
             });
             //删除
             $('#config_delete').click(function () {
                 $.delete('/configs/' + click_config_id, function () {
-                    //TODO
+                    $.msg_success("删除成功");
+                    reLoad();
                 })
             })
         } else {
             $('#dialog_field_title').text('添加规则');
+            $('#dialog_config_name').val('');
+            $('#dialog_config_desc').val('');
+            $('#dialog_config_reg').val('');
             $('#config_delete').hide();
             //add
             $('#dialog_config_submit').click(function () {
@@ -46,15 +49,22 @@ $(document).ready(function () {
                     'description': $('#dialog_config_desc').val(),
                     'expression': $('#dialog_config_reg').val()
                 };
-                $.push('/configs/', data, function () {
-                    //TODO
+                $.post('/configs/', data, function () {
+                    $.msg_success("添加成功");
+                    reLoad();
                 });
             });
         }
     });
 });
+//重新加载该页面
+function reLoad() {
+    $('#addConfigDialog').modal('hide');
+    initPagination();
+}
 //初始化分页
 function initPagination() {
+    $('#pagination').twbsPagination('destroy');
     getConfig(0, size, function (data) {
         $('#pagination').twbsPagination({
             totalPages: data.totalPages,
@@ -73,9 +83,6 @@ function initPagination() {
                 });
             }
         });
-        for (var i in data.content) {
-            addConfigRowHtml(data.content[i]);
-        }
     });
 }
 //ajax 之后,把 数据转换成 html, 添加到页面
@@ -84,19 +91,20 @@ function addConfigRowHtml(item_data) {
 }
 //解析 ajax ,返回每行数据
 function getConfigRowItem(data) {
-    var item_html = $("<div></div>");
+    var item_html = $("#config_item_template").clone();
     item_html.attr('id', getConfigViewId(data.id));
-    var item = $("<div></div>");
-    item.addClass('config_item');
-    var item_title = item.clone();
-    item_title.text(data.name);
-    item_html.append(item_title);
+    item_html.addClass('config_item');
+    var config_name = item_html.find('.config_name');
+    var config_desc = item_html.find('.config_desc');
+    config_name.text(data.name);
+    config_desc.text(data.description);
+    item_html.show();
     //跳转到详情
     item_html.click(function () {
         click_config_id = data.id;
         $('#addConfigDialog').modal('show');
     });
-    return item_html
+    return item_html;
 }
 //获取 控件 id
 function getConfigViewId(id) {
