bf692f166683fd0ff84f5efcac0f5ca60de6fff2
blanke <blankeeee@gmail.com>
2017-05-04 00:04:31 +0800

    [IMP] font local

--- src/main/resources/static/admin/js/task_detail.js
+++ src/main/resources/static/admin/js/task_detail.js
@@ -1,37 +1,68 @@
 /**
  * Created by blanke on 2017/4/4.
  */
+var task_expire = false;//任务是否失效,即超过了截止时间
 $(function () {
     $('#back').click(function () {
         $('#menuFrame', parent.document.body).attr('src', 'task_list.html')
     });
     var task_id = localStorage.getItem('click_task_id');
+    $('#content_row_template').hide();//hide template
+    //点击修改按钮,跳转到修改表单页面 task_add.html
     $('#update').click(function () {
         $('#menuFrame', parent.document.body).attr('src', 'task_add.html')
     });
     if (task_id) {
         //获得任务详情
         $.get('/task/' + task_id, function (data) {
-            $('#task_title').val(data.title);
-            $('#task_desc').val(data.description);
-            $('#task_start_time').val(data.publishTime);
-            $('#task_end_time').val(data.deadlineTime);
+            $('#task_title').text(data.title);
+            $('#task_desc').text(data.description);
+            $('#task_start_time').text($.formatDate(data.publishTime));
+            $('#task_end_time').text($.formatDate(data.deadlineTime));
             $('#user_count').text(data.userCount);
             $('#content_count').text(data.contentCount);
             $('#wait_content_count').text(data.waitContentCount);
             $('#pass_content_count').text(data.passContentCount);
+            task_expire = new Date() > new Date(data.deadlineTime);
+            if (task_expire) {
+                $('#update').hide();
+                $('#delete').hide();//隐藏修改 删除按钮
+                $.msg_waring("任务已经超过截止时间,只能查看数据")
+            }
             //导出数据
             $('#export_data').click(function () {
                 window.open('/tasks/' + task_id + '/contents/export');
             });
             loadContntData();
         });
-
+        //获得收藏状态
+        initLikeState();
     }
+    //初始化收藏状态
+    function initLikeState() {
+        $.get('/tasks/' + task_id + '/is-like', function () {
+            $('#is_like').text("取消收藏");
+            $('#is_like').unbind("click");
+            $('#is_like').click(function () {
+                $.delete('/tasks/' + task_id + '/likes/', function () {
+                    initLikeState();
+                });
+            });
+        }, function () {
+            $('#is_like').text("收藏");
+            $('#is_like').unbind("click");
+            $('#is_like').click(function () {//点击收藏
+                $.post('/tasks/' + task_id + '/likes/', null, function () {
+                    initLikeState();
+                });
+            });
+        });
+    }
+
     //ajax 获得用户提交的数据, 可能是进入页面调用,也可能是删除或提交内容之后调用
     function loadContntData() {
         //get task fields
-        // var div_fields = $('#field_div');
+        var div_fields = $('#field_div');
         var div_contents = $('#contents_div');
         div_contents.empty();//删除子元素
         var row_count = 0;
@@ -52,6 +83,11 @@ $(function () {
         });
     }
 
+    //获得 tag 的 html
+    function getTagHtml() {
+        return $('<div class="label content_tag"></div>');
+    }
+
 
     //根据后端的 field id 生成前端的 dom id
     function getFieldId(fieldId) {
@@ -63,38 +99,31 @@ $(function () {
         return 'content-' + contetnId;
     }
 
-    //返回 field 对应的 item html
-    function getFieldItem(field) {
-        field_html = $("<span>" + field.name + "</span>");
-        field_html.attr('id', getFieldId(field.id));
-        field_html.attr('field_id', field.id);
-        field_html.attr('config_id', field.config.id);
-        field_html.attr('config_expression', field.config.expression);
-        return field_html;
-    }
-
-    //row_index是提交内容第几行
-    function getContentItem(contentItem, row_index) {
-        content_html = $('<input class="form-control input_text attr_value">');
-        content_html.attr('id', getContentId(contentItem.id));
-        content_html.attr('field_id', contentItem.field.id);
-        content_html.attr('content_id', contentItem.id);
-        content_html.attr('row_index', row_index);
-        content_html.val(contentItem.value);
-        return content_html;
-    }
-
     // 每条 content 的 html
     function getContentRow(task_id, content, row_index) {
         row_html = $('#content_row_template').clone();
-        console.log(content)
         row_html.attr('id', getContentRowId(row_index));
         row_html.find('.div_user').text(content.user.nickName);
-        row_html.find('.div_time').text(content.updatedAt);
-        // row_html.find('.content_row_verify').text(content.isVerify);
-        if (content.state != 0) {//已经通过或拒绝
-            $('#verify_bus').hide()
+        row_html.find('.div_time').text($.formatDate(content.updatedAt));
+        var verify_bus = row_html.find('.verify_bus');//审核按钮组
+        var content_state = row_html.find('.content_state');//状态 tag 的 div
+        if (content.state != 0 || task_expire) {//已经通过或拒绝
+            verify_bus.hide();//已经审核过了,不需要显示审核按钮了
         }
+        //显示状态信息,未审核  已通过,已驳回
+        var submit_tag = getTagHtml();
+        if (content.state == 1) {
+            submit_tag.text("已通过");
+            submit_tag.addClass('label-info');
+        } else if (content.state == -1) {
+            submit_tag.text("已驳回");
+            submit_tag.addClass('label-warning');
+        } else {
+            submit_tag.text("未审核");
+            submit_tag.addClass('label-info');
+        }
+        content_state.append(submit_tag);
+
         template = row_html.find('.content_item');
         template_item = template.clone();
         template.hide();
@@ -121,39 +150,12 @@ $(function () {
         var verifyListener = function () {
             var isPass = $(this).hasClass('content_pass');
             $.get("/contents/" + content.id + "?pass=" + isPass, function () {
-
+                $.msg_success("操作成功");
             });
         };
         bu_pass.click(verifyListener);
         bu_dismiss.click(verifyListener);
         return row_html;
-    }
-
-    //检查所有输入是否合法,不合法弹出提示
-    function isJsVerify(row_id) {
-        var error_inputs = $('#' + row_id).find('div.has-error');
-        if (error_inputs.length == 0) {
-            return true;
-        }
-        error_inputs.each(function (i) {
-            $(this).children('input').tooltip("show");
-        });
-        return false;
-    }
-
-    //获得 content 用户输入的内容,发送给服务端
-    function getContentData(row_id) {
-        var input_contents = $('#' + row_id).find('input[field_id]');
-        var data = [];
-        input_contents.each(function (i) {
-            var field_id = $(this).attr('field_id');
-            var val = $(this).val();
-            data.push({
-                'fieldId': field_id,
-                'value': val
-            });
-        });
-        return {'data': data};
     }
 
     function getContentRowId(row_index) {
@@ -162,11 +164,12 @@ $(function () {
 
     //删除任务
     $('#delete').click(function () {
-        var r=confirm("确定删除?");
-        if (r==false){
+        var r = confirm("确定删除?");
+        if (r == false) {
             return;
         }
         $.delete('/tasks/' + task_id, function () {
+            $.msg_success("删除成功");
             $('#back').click();
         })
     });
